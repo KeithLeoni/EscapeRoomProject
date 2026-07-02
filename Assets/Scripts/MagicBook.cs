a6dccea1dee6d37eb6e2be988c7a5a5d6d0517a1
KeithLeoni <keith.leoni03@gmail.com>
2026-07-02 12:28:02 +0200

    Final room

--- Assets/Scripts/MagicBook.cs
+++ Assets/Scripts/MagicBook.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using System.Collections;
+using System.Numerics;
 
 public class MagicBook : MonoBehaviour
 {  
     private bool writtenWithMagicInk = false; 
+    public AudioClip writingSound; 
+    private AudioSource audioSource;
+    public Transform dest;
+
+    public Transform playerRig; 
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,12 +25,24 @@ public class MagicBook : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other) 
-{
-    // Questo catturerà il trigger anche se l'oggetto è cinematico
-    if (other.gameObject.name.Contains("MagicInkTip") || 
-        (other.transform.parent != null && other.transform.parent.name.Contains("MagicInkTip")))
     {
-        Debug.Log("YOU ESCAPED!");
+        // Questo catturerà il trigger anche se l'oggetto è cinematico
+        if (other.gameObject.name.Contains("MagicInkTip") || (other.transform.parent != null && other.transform.parent.name.Contains("MagicInkTip")))
+        {
+            audioSource.PlayOneShot(writingSound);
+            Debug.Log("YOU ESCAPED!");
+
+            CharacterController cc = playerRig.GetComponent<CharacterController>();
+            if(cc != null) cc.enabled = false;
+
+            // Teleport the rig to the new room's coordinates
+            playerRig.position = dest.position;
+
+            // Re-enable character controller
+            if(cc != null) cc.enabled = true;
+
+        }
     }
-}
+
+
 }
